e732a04b6f104aeb3200083f180839c85a08b8f6
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-04-20 16:32:57 +0530

    first commit

---
+++ HandTracking.py
@@ -0,0 +1,55 @@
+import cv2
+import mediapipe as mp
+import math
+
+class HandDetector:
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            static_image_mode=self.mode,  # Set to False for video input (Dynamic)
+            max_num_hands=self.maxHands,  # Maximum number of hands to detect
+            min_detection_confidence=self.detectionCon,  # Confidence for detection
+            min_tracking_confidence=self.trackCon  # Confidence for tracking
+        )
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        allHands = []
+        h, w, c = img.shape
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                hand = {}
+                lmList = []
+                for id, lm in enumerate(handLms.landmark):
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append((cx, cy))
+
+                hand['lmList'] = lmList
+                allHands.append(hand)
+
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+
+        return allHands, img
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = p1
+        x2, y2 = p2
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (x1, y1), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 5, (0, 0, 255), cv2.FILLED)
+
+        return length, [x1, y1, x2, y2], img
---
+++ main.py
@@ -0,0 +1,110 @@
+import cv2
+from HandTracking import HandDetector
+
+# ---------- Button Class ----------
+class Button:
+    def __init__(self, pos, width, height, value):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.value = value
+
+    def draw(self, img):
+        # Draw button box
+        cv2.rectangle(img, self.pos,
+                      (self.pos[0] + self.width, self.pos[1] + self.height),
+                      (225, 225, 225), cv2.FILLED)
+        # Border
+        cv2.rectangle(img, self.pos,
+                      (self.pos[0] + self.width, self.pos[1] + self.height),
+                      (50, 50, 50), 3)
+        # Text
+        cv2.putText(img, self.value,
+                    (self.pos[0] + 30, self.pos[1] + 70),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (50, 50, 50), 3)
+
+    def isClicked(self, x, y):
+        if self.pos[0] < x < self.pos[0] + self.width and \
+           self.pos[1] < y < self.pos[1] + self.height:
+            return True
+        return False
+
+# ---------- Setup ----------
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+# Calculator UI buttons
+buttonValues = [['7', '8', '9', '/'],
+                ['4', '5', '6', '*'],
+                ['1', '2', '3', '-'],
+                ['0', '.', '=', '+']]
+
+buttons = []
+for y in range(4):
+    for x in range(4):
+        xpos = x * 100 + 800
+        ypos = y * 100 + 150
+        buttons.append(Button((xpos, ypos), 100, 100, buttonValues[y][x]))
+
+# Variables
+equation = ""
+delayCounter = 0
+
+# ---------- Main Loop ----------
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    # Find hands
+    hands, img = detector.findHands(img)
+
+    # Draw all buttons
+    for button in buttons:
+        button.draw(img)
+
+    # Display equation
+    cv2.rectangle(img, (800, 70), (1200, 150), (255, 255, 255), cv2.FILLED)
+    cv2.rectangle(img, (800, 70), (1200, 150), (50, 50, 50), 3)
+    cv2.putText(img, equation, (810, 130), cv2.FONT_HERSHEY_PLAIN,
+                3, (0, 0, 0), 3)
+
+    # Hand logic
+    if hands:
+        lmList = hands[0]['lmList']
+        x, y = lmList[8]  # index finger tip
+
+        # Detect click (index and middle finger close)
+        length, _, img = detector.findDistance(lmList[8], lmList[12], img, draw=False)
+
+        if length < 40 and delayCounter == 0:
+            for button in buttons:
+                if button.isClicked(x, y):
+                    value = button.value
+                    if value == "=":
+                        try:
+                            equation = str(eval(equation))
+                        except:
+                            equation = "Error"
+                    else:
+                        equation += value
+                    delayCounter = 1
+
+    # Delay to avoid multi-clicks
+    if delayCounter != 0:
+        delayCounter += 1
+        if delayCounter > 10:
+            delayCounter = 0
+
+    # Show the frame
+    cv2.imshow("Virtual Calculator", img)
+    key = cv2.waitKey(1)
+    if key == ord('c'):
+        equation = ""
+    elif key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
